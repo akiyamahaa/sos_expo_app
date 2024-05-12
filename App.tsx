6bff6d70512114cc2011edd6b62c8cc6cfd1cfd5
Quang Akiyama <quanganhcf2@gmail.com>
2024-05-12 09:40:41 +0700

    update version

--- App.tsx
+++ App.tsx
@@ -1,40 +1,38 @@
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { Provider } from 'react-redux';
-import * as Font from 'expo-font';
-import store from 'redux/stores';
-import GlobalStyles from 'utils/styles';
-import AppLoading from 'expo-app-loading';
-import { Asset } from 'expo-asset';
-import { useFonts } from '@expo-google-fonts/inter';
-import RootStack from 'navigation/RootStack';
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider } from "native-base";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import * as Font from "expo-font";
+import GlobalStyles from "utils/styles";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import { useFonts } from "@expo-google-fonts/inter";
+import RootStack from "navigation/RootStack";
 const MyTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: '#fff',
-    primary: 'rgb(255, 45, 85)',
+    background: "#fff",
+    primary: "rgb(255, 45, 85)",
   },
 };
 
 const listImage = [
-  require('assets/images/bg.png'),
-  require('assets/images/baoluc/concept.png'),
-  require('assets/images/baoluc/formal.png'),
-  require('assets/images/baoluc/harm.png'),
-  require('assets/images/baoluc/prevent.png'),
-  require('assets/images/baoluc/reason.png'),
-  require('assets/images/rubber/signal.png'),
-  require('assets/images/rubber/should.png'),
-  require('assets/images/rubber/should_not.png'),
-  require('assets/images/traffic/concept.png'),
-  require('assets/images/traffic/reason.png'),
-  require('assets/images/traffic/prevent.png'),
-  require('assets/images/traffic/solution.png'),
+  require("assets/images/bg.png"),
+  require("assets/images/baoluc/concept.png"),
+  require("assets/images/baoluc/formal.png"),
+  require("assets/images/baoluc/harm.png"),
+  require("assets/images/baoluc/prevent.png"),
+  require("assets/images/baoluc/reason.png"),
+  require("assets/images/rubber/signal.png"),
+  require("assets/images/rubber/should.png"),
+  require("assets/images/rubber/should_not.png"),
+  require("assets/images/traffic/concept.png"),
+  require("assets/images/traffic/reason.png"),
+  require("assets/images/traffic/prevent.png"),
+  require("assets/images/traffic/solution.png"),
 ];
 
 export default function App() {
@@ -46,7 +44,7 @@ export default function App() {
   const loadResources = async () => {
     await Promise.all([
       Font.loadAsync({
-        'Chewy-Regular': require('./assets/font/Chewy-Regular.ttf'),
+        "Chewy-Regular": require("./assets/font/Chewy-Regular.ttf"),
       }),
       Asset.loadAsync(listImage),
     ]);
@@ -63,15 +61,13 @@ export default function App() {
 
   return (
     <SafeAreaProvider style={[styles.root, GlobalStyles.AndroidSafeArea]}>
-      <Provider store={store}>
-        <NativeBaseProvider>
-          <NavigationContainer theme={MyTheme}>
-            <SafeAreaView />
-            <RootStack />
-            <StatusBar style="auto" />
-          </NavigationContainer>
-        </NativeBaseProvider>
-      </Provider>
+      <NativeBaseProvider>
+        <NavigationContainer theme={MyTheme}>
+          <SafeAreaView />
+          <RootStack />
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </NativeBaseProvider>
     </SafeAreaProvider>
   );
 }
@@ -79,6 +75,6 @@ export default function App() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    position: 'relative',
+    position: "relative",
   },
 });
